58927927a1448fae8f95017d1194022463ce4e4a
unknown <ulyana250188@gmail.com>
2019-06-14 23:18:26 +0300

    Updates

---
+++ precache-manifest.471a27623ad878e770239c20a40af91c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b477474844bb396e049c3614bf20d08a",
+    "url": "/Calendar-React/index.html"
+  },
+  {
+    "revision": "4115d0aa86683c2655f2",
+    "url": "/Calendar-React/static/css/main.9a333b93.chunk.css"
+  },
+  {
+    "revision": "83d9fd4ff40ac70b65e3",
+    "url": "/Calendar-React/static/js/2.fa8a02f4.chunk.js"
+  },
+  {
+    "revision": "4115d0aa86683c2655f2",
+    "url": "/Calendar-React/static/js/main.0ba7e447.chunk.js"
+  },
+  {
+    "revision": "4f30b3fb61253f1739ab",
+    "url": "/Calendar-React/static/js/runtime~main.2ea34867.js"
+  }
+]);
